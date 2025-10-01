c86005c2b48f64cd967969b69c4d22fbf7fcb181
libernalii <alinaliberna@gmail.com>
2025-10-01 20:31:22 +0300

    productData

---
+++ TeamApp/TeamApp/ProductManager.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using TeamApp.Models;
+
+namespace TeamApp
+{
+    public class ProductManager
+    {
+        private readonly string _filePath;
+
+        public ProductManager(string filePath)
+        {
+            _filePath = filePath;
+        }
+
+        public void SaveProduct(Product product)
+        {
+            List<Product> products = LoadProducts();
+            products.Add(product);
+
+            string json = JsonSerializer.Serialize(products, new JsonSerializerOptions { WriteIndented = true });
+            File.WriteAllText(_filePath, json);
+        }
+
+        public List<Product> LoadProducts()
+        {
+            if (!File.Exists(_filePath))
+                return new List<Product>();
+
+            string json = File.ReadAllText(_filePath);
+            return JsonSerializer.Deserialize<List<Product>>(json) ?? new List<Product>();
+        }
+    }
+
+}
--- TeamApp/TeamApp/Program.cs
+++ TeamApp/TeamApp/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using TeamApp.Models;
+using TeamApp;
+
+class Program
+{
+    static void Main()
+    {
+        var manager = new ProductManager("products.json");
+
+        Console.Write("Enter product name: ");
+        string name = Console.ReadLine();
+
+        Console.Write("Enter price product: ");
+        decimal price = Convert.ToDecimal(Console.ReadLine());
+
+        Console.Write("Enter quantity products: ");
+        int quantity = Convert.ToInt32(Console.ReadLine());
+
+        var product = new Product
+        {
+            Name = name,
+            Price = price,
+            Quantity = quantity
+        };
+
+        manager.SaveProduct(product);
+
+        Console.WriteLine("List products:");
+
+        var products = manager.LoadProducts();
+        foreach (var p in products)
+        {
+            Console.WriteLine($"• {p.Name} | price: {p.Price} | quantity: {p.Quantity}");
+        }
+    }
+}
+
