b2f3fe7aed8836ce3e4b3b467a57c32d6e1e53bb
bogdanch11k <bolijnik50@gmail.com>
2025-10-01 20:30:14 +0300

    Samostijne

---
+++ TeamApp/TeamApp/Product.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeamApp
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public int Count { get; set; }
+
+        public Product(int id, string name, decimal price, int count)
+        {
+            id = Id;
+            name = Name;
+            price = Price;
+            count = Count;
+        }
+        public override string ToString()
+        {
+            return $"Product: {Name}\nPrice: {Price}\nCount: {Count}";
+        }
+    }
+}
