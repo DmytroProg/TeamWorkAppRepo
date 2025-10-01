f14455b5ac25d0fed6fdf982123f29d3904e5e9d
bogdanch11k <bolijnik50@gmail.com>
2025-10-01 19:58:48 +0300

    First team project

--- TeamApp/TeamApp/Program.cs
+++ TeamApp/TeamApp/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Text.RegularExpressions;
+
+Console.Write("Enter your name: ");
+string fullName = Console.ReadLine();
+
+string pattern = @"^[АЯІЇЄ][аяіїє]+\s[АЯІЇЄ][аяіїє]+$";
+bool isTrue = Regex.IsMatch(fullName, pattern);
+
+if (isTrue = true)
+{
+    Console.WriteLine($"Привiт {fullName}!");
+}
+else if (isTrue = false)
+{
+    Console.WriteLine("Помилка при введенні даних");
+}
+else
+{
+    Console.WriteLine("Error");
+}
