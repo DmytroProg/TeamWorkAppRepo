090682443bfc080ad490b0286c0dd95863574b06
Toffy-X64 <dimaphp027@gmail.com>
2025-10-01 20:33:55 +0300

    adding-product-db

--- TeamApp/TeamApp/Program.cs
+++ TeamApp/TeamApp/Program.cs
@@ -1,3 +1,5 @@
 ﻿using TeamApp;
 
 Task5Service.Init();
+
+ProductService.ProductMainEntryAsync(@"D:\Desk\IT-S\NetworkProgramming\Lesson_6\TeamWork\TeamWorkAppRepo\TeamApp\context.stor").GetAwaiter().GetResult();
