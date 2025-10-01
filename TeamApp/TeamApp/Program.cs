14bc3b0d82f0cabd171060ea9764a9698681b125
libernalii <alinaliberna@gmail.com>
2025-10-01 20:01:02 +0300

    Multiplication table

--- TeamApp/TeamApp/Program.cs
+++ TeamApp/TeamApp/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter number: ");
+        int input = Convert.ToInt32(Console.ReadLine());
+
+        if (input > 0)
+        {
+            Console.WriteLine($"\nMultiplication table for {input}:");
+            for (int i = 1; i <= 10; i++)
+            {
+                int result = input * i;
+                Console.WriteLine($"{input} × {i} = {result}");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Invalid number.");
+        }
+
+        Console.WriteLine("\nPress any key for exit...");
+        Console.ReadKey();
+    }
+}
